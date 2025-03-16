b360a59516525c6fd72df900914300cfcfa8d7ec
iHarshSinha <harsh.sinha@iiitb.ac.in>
2025-03-16 13:35:24 +0530

    cors change

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let app = express();
 app.use(express.urlencoded({extended:true,limit: '50mb'}))
 app.use(express.json({limit: '50mb'}))
 app.use(methodOverride("_method"))
-app.use(cors())
+app.use(cors(origin="*"))
 
 
 
